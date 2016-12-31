87291f5061fe1da1963dbc407b50840c757a573c
Mike Cavaliere <mike@cavaliere.org>
2016-12-30 23:59:18 -0500

    Form submission basics

--- src/components/pages/New.js
+++ src/components/pages/New.js
@@ -1,12 +1,45 @@
 import React from 'react';
 import {connect} from 'react-redux';
+import {Button, FormGroup, FormControl, ControlLabel, HelpBlock} from 'react-bootstrap';
 
 class NewPagePage extends React.Component {
+	constructor(props) {
+	  super(props);
+
+      this.state = {
+        value: ''
+      };
+    }
+
+	handleChange(e) {
+		this.setState({
+			value: e.target.value
+		})
+	}
+
+	handleSubmit(e) {
+		e.preventDefault();
+		
+	}
+
 	render() {
 		return (
 			<div className="container-fluid">
 				<h1>Create New Post: </h1>
-
+				<form action="" onSubmit={this.handleSubmit}>
+					<FormGroup controlId="formControlsTextarea">
+						<ControlLabel>Enter a message:</ControlLabel>
+						<FormControl
+						  componentClass="textarea"
+						  placeholder="textarea"
+						  onChange={this.handleChange.bind(this)}
+						  value={this.state.value}
+			            />
+					</FormGroup>
+					<Button type="submit">
+				      Submit
+				    </Button>
+				</form>
 			</div>
 		);
 	}
