b92e3bd6d828326ebf208a4573c1cd32e914bd49
Mike Cavaliere <mike@cavaliere.org>
2017-01-11 22:45:29 -0500

    Adds file upload field with working preview

--- src/components/guestbook/New.js
+++ src/components/guestbook/New.js
@@ -10,14 +10,40 @@ class NewMessagePage extends React.Component {
 	  super(props, context);
 
       this.state = {
-        value: this.props.value || ''
+        value: this.props.value || '',
+		previewSrc: null,
+		showPreview: false
       };
     }
 
-	handleChange(e) {
+	handleTextChange(e) {
 		this.setState({
 			value: e.target.value
 		})
+	}
+
+	handleFileChange(e) {
+		console.warn("handleFileChange: ", e.target.files);
+		// this.setState({
+		// 	file: e.target.value
+		// })
+		var file   = e.target.files[0];
+		var reader = new FileReader();
+
+		reader.addEventListener("load", () => {
+			console.warn("LOADED: ", arguments);
+			this.setState({
+				previewSrc: reader.result,
+				showPreview: true
+			});
+
+		});
+
+		if (file) {
+			reader.readAsDataURL(file);
+		}
+
+
 	}
 
 	handleSubmit(e) {
@@ -25,23 +51,44 @@ class NewMessagePage extends React.Component {
 
 		this.props.actions.create({ body: this.state.value }).then(() => {
 			browserHistory.push('/guestbook');
-		})
+		});
 	}
 
 	render() {
+		let preview = null;
+
+		if (this.state.showPreview === true) {
+			preview = <div className="preview well"><img src={this.state.previewSrc} /></div>;
+		}
+
+
+
+
 		return (
 			<div className="container-fluid">
 				<h1>Leave a Message </h1>
+
 				<form action="" onSubmit={this.handleSubmit.bind(this)}>
 					<FormGroup controlId="formControlsTextarea">
 						<ControlLabel>Enter a message:</ControlLabel>
 						<FormControl
 						  componentClass="textarea"
 						  placeholder="textarea"
-						  onChange={this.handleChange.bind(this)}
+						  onChange={this.handleTextChange.bind(this)}
 						  value={this.state.value}
 			            />
 					</FormGroup>
+					<FormGroup controlId="formControlsFile">
+				      <ControlLabel>Upload a Photo</ControlLabel>
+				      <FormControl
+						  type="file"
+						  id="formControlsFile"
+						  onChange={this.handleFileChange.bind(this)}
+					  />
+				    </FormGroup>
+
+					{preview}
+
 					<Button type="submit">
 				      Submit
 				    </Button>
