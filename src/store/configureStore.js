179403fae4b1f71793ba9f6c2b047b859fdde5e0
Mike Cavaliere <mike@cavaliere.org>
2017-04-28 21:53:52 -0400

    Adds redux logger

--- src/store/configureStore.js
+++ src/store/configureStore.js
@@ -1,10 +1,12 @@
 import {createStore, applyMiddleware} from 'redux';
 import rootReducer from '../reducers/rootReducer';
+import logger from 'redux-logger';
 import thunk from 'redux-thunk';
 
 export default function configureStore() {
 	return createStore(
 		rootReducer,
+		applyMiddleware(logger),
 		applyMiddleware(thunk)
 	);
 }
